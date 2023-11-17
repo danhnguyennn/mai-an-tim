dea2436d8d25f29bb208293df58f6e5becfc3f8f
danhnguyennn <88915356+danhnguyennn@users.noreply.github.com>
2023-11-17 17:01:38 +0700

    fix

--- models/captcha.py
+++ models/captcha.py
@@ -1,7 +1,7 @@
 from onest_captcha import OneStCaptchaClient
 
 def bypassCaptcha(APIKEY):
-    for i in range(3):
+    for i in range(2):
         client = OneStCaptchaClient(apikey=APIKEY)
         result = client.recaptcha_v2_task_proxyless(site_url="https://traodoisub.com/view/chtiktok/", site_key="6LeGw7IZAAAAAECJDwOUXcriH8HNN7_rkJRZYF8a", invisible=False)
         try:
